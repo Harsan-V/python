2ae292feaea641a4bdcb978f92b522997a771a7a
Harshith V <88479859+MaddedJack@users.noreply.github.com>
2025-06-23 13:44:08 +0530

    Add files via upload

---
+++ tcsprob1.py
@@ -0,0 +1,11 @@
+a = int(input("Enter any num1 : "))
+b = int(input("Enter any num2 : "))
+c = int(input("Enter any num3 : "))
+lis1 = [int (i) for i in str(a)]
+lis2 = [int (i) for i in str(b)]
+lis3 = [int (i) for i in str(c)]
+sum = 0
+x = max(lis1)+max(lis2)+max(lis3)
+y = min(lis1)+min(lis2)+mib(lis3)
+print(x+y)
+    
